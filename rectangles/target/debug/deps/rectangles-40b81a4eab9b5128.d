dfb0351fd0559d1a02caa16a8e9be9be6771f226
would <dchessari@hotmail.com>
2025-03-04 10:47:59 +1100

    create

---
+++ rectangles/target/debug/deps/rectangles-40b81a4eab9b5128.d
@@ -0,0 +1,9 @@
+/Users/danielchessari/Rust-Learning/rectangles/target/debug/deps/librectangles-40b81a4eab9b5128.rmeta: src/main.rs Cargo.toml
+
+/Users/danielchessari/Rust-Learning/rectangles/target/debug/deps/rectangles-40b81a4eab9b5128.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ rectangles/target/debug/deps/rectangles-ca24bec4a9c620ea.d
@@ -0,0 +1,9 @@
+/Users/danielchessari/Rust-Learning/rectangles/target/debug/deps/librectangles-ca24bec4a9c620ea.rmeta: src/main.rs Cargo.toml
+
+/Users/danielchessari/Rust-Learning/rectangles/target/debug/deps/rectangles-ca24bec4a9c620ea.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
