a28967b41bee7bb14e260525eaa6fa767ab398f8
would <dchessari@hotmail.com>
2025-03-04 11:35:26 +1100

    update

---
+++ rectangles/target/debug/deps/rectangles-8a24b2e18c630404.d
@@ -0,0 +1,5 @@
+/Users/danielchessari/Rust-Learning/rectangles/target/debug/deps/rectangles-8a24b2e18c630404: src/main.rs
+
+/Users/danielchessari/Rust-Learning/rectangles/target/debug/deps/rectangles-8a24b2e18c630404.d: src/main.rs
+
+src/main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/Users/danielchessari/Rust-Learning/rectangles/target/debug/rectangles: /Users/danielchessari/Rust-Learning/rectangles/src/main.rs
