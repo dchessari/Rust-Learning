3fddda67e3354577f145b579ac23feed5e376526
danches <dchessari@hotmail.com>
2025-03-05 18:36:42 +1100

    up

--- rectangles/target/debug/deps/rectangles-b50def6a073ff734.d
+++ rectangles/target/debug/deps/rectangles-b50def6a073ff734.d
@@ -1,5 +1,5 @@
-/Users/danielchessari/Rust-Learning/rectangles/target/debug/deps/librectangles-b50def6a073ff734.rmeta: src/main.rs
+/Users/dan/Rust-Learning/rectangles/target/debug/deps/librectangles-b50def6a073ff734.rmeta: src/main.rs
 
-/Users/danielchessari/Rust-Learning/rectangles/target/debug/deps/rectangles-b50def6a073ff734.d: src/main.rs
+/Users/dan/Rust-Learning/rectangles/target/debug/deps/rectangles-b50def6a073ff734.d: src/main.rs
 
 src/main.rs:
--- rectangles/target/debug/rectangles.d
+++ rectangles/target/debug/rectangles.d
@@ -1,1 +1,1 @@
-/Users/danielchessari/Rust-Learning/rectangles/target/debug/rectangles: /Users/danielchessari/Rust-Learning/rectangles/src/main.rs
+/Users/dan/Rust-Learning/rectangles/target/debug/rectangles: /Users/dan/Rust-Learning/rectangles/src/main.rs
