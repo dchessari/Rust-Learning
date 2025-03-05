64107b73d550db92ccb00ece0e7692991fff26c4
danches <dchessari@hotmail.com>
2025-03-05 17:48:05 +1100

    down

---
+++ rectangles/target/debug/deps/rectangles-ace9e01b06a80cb6.d
@@ -0,0 +1,9 @@
+/Users/dan/Rust-Learning/rectangles/target/debug/deps/librectangles-ace9e01b06a80cb6.rmeta: src/main.rs Cargo.toml
+
+/Users/dan/Rust-Learning/rectangles/target/debug/deps/rectangles-ace9e01b06a80cb6.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ rectangles/target/debug/deps/rectangles-d5e0f5bf03f4a9c0.d
@@ -0,0 +1,9 @@
+/Users/dan/Rust-Learning/rectangles/target/debug/deps/librectangles-d5e0f5bf03f4a9c0.rmeta: src/main.rs Cargo.toml
+
+/Users/dan/Rust-Learning/rectangles/target/debug/deps/rectangles-d5e0f5bf03f4a9c0.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
