d920c77c53305c52818da85915a96809425478d6
would <dchessari@hotmail.com>
2025-03-21 16:57:11 +1100

    21/3/25

--- pig-latin/src/main.rs
+++ pig-latin/src/main.rs
@@ -1,5 +1,33 @@
-fn main() {
+fn check_word (original: &str) -> &str {
+    // sort string into words that start with vowels and words that start with consts
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    let cons = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l' 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
 
+    for original.starts_with(&vowels'-') {
+
+    }
+}
+
+fn vowel_start() {
+    // how to handle words that start with vowels
+
+}
+
+fn cons_start() {
+    // how to handle words that start with consts
+
+}
+
+fn suffix() {
+    // update original string to be in pig latin
+
+}
+
+fn main() {
+    // define the string
+    let mut original = String::from("I want to be six feet tall when I grow up");
+
+    // print updated string
 }
 
 
